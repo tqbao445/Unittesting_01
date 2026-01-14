2d7c4840f7d4a6598229282e6de86f4cef62258f
tqbao445 <tqbao4205@gmail.com>
2026-01-14 19:19:51 +0700

    modify build.xml

--- src/com/tqbao/mathutility/core/MathUtil.java
+++ src/com/tqbao/mathutility/core/MathUtil.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.tqbao.mathutility.core;
 
-/**
- *
- * @author tqbao4205
- */
 public class MathUtil {
     public static long getFactorial(int n) {
         if(n < 0 || n > 20)
