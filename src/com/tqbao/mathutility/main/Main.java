3e3bb1a5e8d377fde49005400090ef38099c67d9
tqbao445 <tqbao4205@gmail.com>
2026-01-15 19:03:50 +0700

    hihi

--- src/com/tqbao/mathutility/main/Main.java
+++ src/com/tqbao/mathutility/main/Main.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.tqbao.mathutility.main;
 
 import com.tqbao.mathutility.core.MathUtil;
 
-/**
- *
- * @author tqbao4205
- */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(MathUtil.getFactorialRecursion(5));
     }
 }
